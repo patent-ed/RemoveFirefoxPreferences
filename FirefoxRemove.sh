5ff05324f648a0b73eaa5ebaaa1663914cf34195
Ed <41171233+patent-ed@users.noreply.github.com>
2022-08-19 13:42:01 -0400

    Create FirefoxRemove.sh

---
+++ FirefoxRemove.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# 2022.08.19 - This should be run from self service
+
+
+# Define Variables
+firefoxapp="/Applications/Firefox.app"
+mozilla_system_plist="/Library/Preferences/org.mozilla.firefox"
+
+MacUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+
+userPlist="/Users/$MacUser/Library/Preferences/org.mozilla.firefox.plist"
+
+# Kill firefox process
+killall 'firefox'
+
+# remove OEM autoconfig.js file
+if [[ -e "/Users/$MacUser/Library/Preferences/org.mozilla.firefox.plist" ]];then
+	echo "Removing config file."
+    rm -rf '/Users/$MacUser/Library/Preferences/org.mozilla.firefox.plist'
+fi
+
+
+#rm -rf '/Users/$MacUser/Library/Preferences/org.mozilla.firefox.plist'
+
+rm -rf $firefoxapp
+rm -rf $mozilla_system_plist
+
+rm -rf $userPlist
+
